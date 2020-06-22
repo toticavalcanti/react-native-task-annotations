ef43a79c4ae44f6c3b74560c11866c69c06a0a84
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2020-06-21 23:01:06 -0300

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Toti, você é um dev muito fodaaaaaa!!!!!!!</Text>
+      <Text>Testando react native</Text>
     </View>
   );
 }
