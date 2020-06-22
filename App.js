608f0c13315e1d18bc4d90de0d68007f2d09f7e3
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2020-06-21 23:43:24 -0300

    add the text field

--- App.js
+++ App.js
@@ -1,19 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, TextInput, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Testando react native</Text>
+class App extends React.Component{
+  render(){
+    return(
+    <View style={style.viewStyle}>
+      <Text>Olá native!!!</Text>
+      <TextInput 
+        style={style.inputStyle}
+      />
     </View>
-  );
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const style = {
+  viewStyle: {
+    flex: 1, 
+    alignItems: 'center', 
+    justifyContent: 'center'
+  }, 
+
+  inputStyle: {
+    height: 40,
+    width: 300,
+    borderColor: "green",
+    borderWidth: 1
+  }
+}
+
+export default App;
